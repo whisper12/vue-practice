39264018a3f5d9cfa5be4a6dc80d764fd7edf5c1
zhouqiaobo <zhouqiaobo@pipi.cn>
2018-03-12 15:50:03 +0800

    请求地址更改为jsonp

--- src/components/movie/inTheaters.vue
+++ src/components/movie/inTheaters.vue
@@ -50,7 +50,7 @@ export default {
       this.in_theater_body =item.body
       this.in_theaters_data_body_subjects = item.body.subjects
     }else{
-      this.$http.get('/v2/movie/in_theaters',{params:{'city':this.$store.getters.getCity,'count':28}})
+      this.$http.jsonp('https://api.douban.com/v2/movie/in_theaters',{params:{'city':this.$store.getters.getCity,'count':28}})
           .then(function (response) {
             this.loading = false
             this.in_theaters_data = response
